ca5b42feb641cc6473967928ed14c941b0a15167
kaya <fehmikaya@gmail.com>
2018-10-11 14:35:49 +0300

    playground 2

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,24 +6,24 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     }
     console.log('Connected');
     const db = client.db('TodoApp');
-    // db.collection('Todos').insertOne( {
-    //     text: 'Something todo',
-    //     completed: false
-    // }, (err, result) => {
-    //     if(err){
-    //         return console.log(err);
-    //     }
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
-    db.collection('Users').insertOne( {
-        name: 'Fehmi',
-        age: 40,
-        location: 'TR'
+    db.collection('Todos').insertOne( {
+        text: 'Something todo',
+        completed: true
     }, (err, result) => {
         if(err){
             return console.log(err);
         }
         console.log(JSON.stringify(result.ops, undefined, 2));
     });
+    // db.collection('Users').insertOne( {
+    //     name: 'Fehmi',
+    //     age: 40,
+    //     location: 'TR'
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log(err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
     client.close();
 });
