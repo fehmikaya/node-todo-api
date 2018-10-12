e024b3a0040ee6cf51ea10950c8c554fcbaa5253
kaya <fehmikaya@gmail.com>
2018-10-12 16:32:44 +0300

    Todo post handler

--- server/server.js
+++ server/server.js
@@ -1,29 +1,26 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
+var {mongoose} = require('./db/mongoose'); 
+var {Todo} = require('./models/todo'); 
+var {User} = require('./models/user'); 
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var app = express();
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    compeletedAt: {
-        type: Number
-    }
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
 });
 
-var newTodo = new Todo({
-    text: 'New Todo',
-    completed: false,
-    compeletedAt: 123
-});
-
-newTodo.save().then((doc) => {
-    console.log(JSON.stringify(doc, undefined, 2));
-}, (err) => {
-    console.log(err);
-});
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+})
