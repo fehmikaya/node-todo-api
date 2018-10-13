fa018e0dcbca3791ce2801295a81d85469a2a768
kaya <fehmikaya@gmail.com>
2018-10-13 12:41:53 +0300

    Heroku setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// heroku config:set MONGOLAB_URI="mongodb://node:Mexican22@ds259111.mlab.com:59111/node-api/todoapp"
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
